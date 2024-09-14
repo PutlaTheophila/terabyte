9978609c47404bff458d3f2c8232949f1272f5de
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-09-14 23:16:15 +0530

    thieteen

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(express.json());
 
 app.use(cors({
     // origin: 'https://sports-iitbhilai.netlify.app/',
-    origin:'http://localhost:5174',
+    // origin:'http://localhost:5174',
+    origin:'https://sports-iitbhilai.netlify.app/',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
     // Allows cookies to be sent
@@ -62,10 +63,11 @@ app.get('/' , (req ,res)=>{
 
 app.get ('/api/v1/auth/google' , passport.authenticate('google',{scope:['profile' ,'email']}))
 app.get('/api/v1/auth/google/callback', passport.authenticate('google' , {
-    failureRedirect:'http://localhost:5174/',
-    successRedirect:'http://localhost:5174/nso'
-    // successRedirect:'/api/v1/host',
-    // failureRedirect:'/'
+    // failureRedirect:'http://localhost:5174/',
+    // successRedirect:'http://localhost:5174/nso'
+
+    failureRedirect:'https://sports-iitbhilai.netlify.app/',
+    successRedirect:'https://sports-iitbhilai.netlify.app/nso'
 
 }) , (req ,res)=>{
     res.status(200).json('thank you for signing in')
