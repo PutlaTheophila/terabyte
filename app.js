d9e68b1912552810ecabe17174efa66897617a1f
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-09-16 03:36:49 +0530

    hello

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ dotenv.config();
 app.use(express.json());
 
 app.use(cors({
-    origin:'http://localhost:5173/',
+    origin:'http://localhost:5173',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true
 }));
