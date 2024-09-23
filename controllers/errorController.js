22a8c7034ac610e4afe7402232216b85c556734a
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-09-23 23:35:45 +0530

    hello

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -9,7 +9,7 @@ const developmentErrors = (res , error) =>{
 }
 
 const productionErrors = ( res , error) =>{
-    console.log('welcome to productionn errorss');
+    console.log('welcome to productionn errors');
     if(error.isOperational){
         console.log('error is operational');
         res.status(error.statusCode).json({
