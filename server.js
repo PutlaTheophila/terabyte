3c1c84c2e0c5ed755298288a0c4248d0818a3252
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-09-24 00:26:46 +0530

    hello

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 mongoose.connect('mongodb+srv://putlat:LjPde0OWQxHLwLrU@cluster0.w4qzg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then((conn)=>{
-    console.log('connected to database 2');
+    console.log('connected to database');
     //starting app
     app.listen(PORT, '0.0.0.0', () => {
         console.log(`Server is running on port ${PORT}`);
