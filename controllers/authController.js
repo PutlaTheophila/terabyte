60298b0c4f1d7270acb01c191f617bc68799ac2b
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-09-29 02:35:03 +0530

    hello

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,7 @@ export const findUser = asyncErrorHandler(async(req ,res , next) =>{
         message:'user logged in',
         data:{
             user:user,
-            sessionId
+            id:sessionId
         }
     })
 })
