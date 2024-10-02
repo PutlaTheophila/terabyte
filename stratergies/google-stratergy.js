6ff0a879157fc3a269968b15b9c099e023421d40
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-02 20:28:21 +0530

    hello

--- stratergies/google-stratergy.js
+++ stratergies/google-stratergy.js
@@ -1,30 +1,31 @@
-import mongoose from "mongoose";
-import {Strategy} from "passport-google-oauth20";
-import passport from "passport";
-import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import {Strategy} from "passport-google-oauth20";
+// import passport from "passport";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
+// let User;
 
+// passport.serializeUser(async(user , done)=>{
+//   console.log('serialise user running .....')
+//   User = user;
+//   done(null ,user);
+// })
+// passport.deserializeUser(async(user , done)=>{
+//   console.log('hello...................')
+//   done(null ,user);
+// })
 
-passport.serializeUser(async(user , done)=>{
-  console.log('serialise user running .....')
-  done(null ,user);
-})
-passport.deserializeUser(async(user , done)=>{
-  console.log('hello...................')
-  done(null ,user);
-})
-
-export default passport.use(
-    new Strategy({
-      clientID :process.env.CLIENT_ID,
-      clientSecret:process.env.CLIENT_SECRET,
-      callbackURL: 'https://terabyte-kvey.onrender.com/api/v1/auth/google/callback',      
-      save:true
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      done(null, profile);
-    })
-);
+// export default passport.use(
+//     new Strategy({
+//       clientID :process.env.CLIENT_ID,
+//       clientSecret:process.env.CLIENT_SECRET,
+//       callbackURL: 'https://terabyte-kvey.onrender.com/api/v1/auth/google/callback',      
+//       save:true
+//     },
+//     async (accessToken, refreshToken, profile, done) => {
+//       done(null, profile);
+//     })
+// );
 
