0721e8d2dc224a91b17d28be5471c465159e24ec
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-03 23:02:38 +0530

    hello

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 // });
 
 app.use(cors({
-    origin:["https://sports13.netlify.app","http://localhost:5176"],
+    origin:'https://sports13.netlify.app',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
     allowedHeaders:['Authorization','Content-Type','Custom-Header']
