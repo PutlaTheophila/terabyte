ce1ada98acdd5ce2cda083c52b4f99faeda19266
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-03 14:44:39 +0530

    hello

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,10 @@ import asyncErrorHandler from "../utils/async-error-handler.js";
 import CustomError from "../utils/customError.js";
 import User from "../models/userModel.js";
 import mongoose from "mongoose";
-import {google} from "googleapis";
-import {OAuth2Client} from "google-auth-library"
+import jwt from 'jsonwebtoken';
+import {OAuth2Client} from "google-auth-library";
+import dotenv from 'dotenv'
+dotenv.config()
  
 
 const client = new OAuth2Client(process.env.CLIENT_ID);
@@ -11,13 +13,17 @@ const client = new OAuth2Client(process.env.CLIENT_ID);
 
 export const getGoogleData = asyncErrorHandler(async(req ,res)=>{
     const {credential} = req.body;
+    console.log(credential);
     const ticket = await client.verifyIdToken({
         idToken: credential,
         audience: process.env.CLIENT_ID,
       });
       const payload = ticket.getPayload();
       const userId = payload['sub'];
-      res.cookie('email',payload.email);
+      const token = jwt.sign({ payload}, process.env.JWT_SECRET, { expiresIn: '1h' });
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log('decoded token', decoded);
+      res.setHeader('Set-Cookie', `authToken=${token}; HttpOnly; Secure; SameSite=None; Max-Age=3600`);
       res.status(200).json({
         status:'success',
         message: 'User verified', 
