e93fb54f827ef0a7474b9a0ddfb2d660fa80ea13
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-05 00:41:57 +0530

    hello

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,7 +23,12 @@ export const getGoogleData = asyncErrorHandler(async(req ,res)=>{
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       console.log('decoded token', decoded);
       res.setHeader('Set-Cookie', `authToken=${token}; Secure; SameSite=None; Max-Age=3600`);
-      res.cookie('authToken',token);
+      res.cookie('authToken',token , {
+        httpOnly: true,       // Makes it inaccessible via JS (optional, if you don't need it on the frontend)
+        secure: true,         // Set to true for HTTPS, false for HTTP (use false for local dev without HTTPS)
+        sameSite: 'None',     // Allow cross-origin cookies
+        maxAge: 3600000,      // Cookie expiration in milliseconds
+      });
       res.status(200).json({
         status:'success',
         message: 'User verified', 
