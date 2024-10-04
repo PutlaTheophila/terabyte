57844feb0e15936845032834fb6a463e509e20bf
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-05 01:11:37 +0530

    hello

--- routes/tournamentsRoute.js
+++ routes/tournamentsRoute.js
@@ -2,9 +2,10 @@ import express from "express";
 const tournamentsRouter = express.Router();
 import { createTournament ,getAllTournaments ,getTournament ,updateTournament } from "../controllers/tournamentsController.js";
 import { getGoogleData, isUserLoggedIn } from "../controllers/authController.js";
+import {verifyIdToken} from "../mw.js"
 
 tournamentsRouter.route('/')
-    .get(getAllTournaments)
+    .get(verifyIdToken,getAllTournaments)
     .post(createTournament)
 tournamentsRouter.route('/:id')
     .patch(updateTournament)
