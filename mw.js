dc879c4053919040d02fb8c588d7c75fc9bbe661
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-05 00:57:47 +0530

    hello

--- mw.js
+++ mw.js
@@ -4,29 +4,28 @@ import CustomError from "./utils/customError.js";
 import jwt from 'jsonwebtoken';
 
 export const verifyIdToken = asyncErrorHandler(async(req , res , next)=>{
-    const cookies = req.cookies;
-    console.log('cookies form mw',cookies);
+    const token = req.cookies.authToken;
+    console.log('cookies form mw',token);
 
     // Check if cookies exist
     if (!cookies) {
       return res.status(401).json({ message: 'Access denied, no cookies provided' });
     }
   
-    // Manually extract the `authToken` cookie (assuming it's a simple key=value format)
-    const token = cookies.split('; ').find(cookie => cookie.startsWith('authToken='));
+    // // Manually extract the `authToken` cookie (assuming it's a simple key=value format)
+    // const token = cookies.split('; ').find(cookie => cookie.startsWith('authToken='));
   
     if (!token) {
       return res.status(401).json({ message: 'Access denied, no auth token found' });
     }
 
-    const authToken = token.split('=')[1];
-    console.log('authToken', authToken);
-    console.log(authToken);
+    // const authToken = token.split('=')[1];
+    console.log('authToken', token);
 
-    const decoded = jwt.verify(authToken, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log(decoded);
     // Attach the decoded token data to the request (can be accessed in routes)
     req.user = decoded;
-    // next();
-    res.json(decoded);
+    next();
+
 })
