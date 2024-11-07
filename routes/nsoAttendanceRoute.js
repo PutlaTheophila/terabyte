4523fb79ce41403e58ea0a5db2b6f4185bdd81ff
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-11-07 21:34:27 +0530

    hello

--- routes/nsoAttendanceRoute.js
+++ routes/nsoAttendanceRoute.js
@@ -1,10 +1,10 @@
 import express from "express"
 const nsoAttendanceRouter = express.Router();
 import { getPlayersForAttendance, getSheetsData } from "../controllers/nsoAttendanceController.js";
-
+import {verifyIdToken} from "../mw.js"
 
 nsoAttendanceRouter.route('/players/:sport')
-    .get(getPlayersForAttendance)
+    .get(verifyIdToken,getPlayersForAttendance)
 
 
 export default nsoAttendanceRouter;
